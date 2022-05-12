c2582cab1502e9f0deff6015e15ccb9b7d846231
Monil Darediya <103734757+MonilDared@users.noreply.github.com>
2022-05-12 15:42:54 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php 
+for($i=1;$i<=5;$i++) {
+  for($j=0;$j<=$i;$j++) {
+  echo " * ";
+  }
+  echo "</br>";
+}
+?> 
+  </body>
+</html>
